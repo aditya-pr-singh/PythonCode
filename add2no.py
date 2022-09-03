93389c4a7c337b98bc06fd19e0068915eccb9638
aditya-pr-singh <mradithaku18@gmail.com>
2022-09-03 18:38:40 +0530

    basic python program using function

---
+++ add2no.py
@@ -0,0 +1,18 @@
+# def myfun(a,b):
+#     return a + b
+# print(myfun(3,5))
+
+
+# def myfun():
+#     print(3+5)
+# myfun()
+
+
+# def myfun(a,b):
+#     print(a+b)
+# myfun(3,5)
+
+
+def myfun():
+    return 3+5
+print(myfun())
---
+++ hwusingfun.py
@@ -0,0 +1,19 @@
+# def myfun(x):
+#     return x
+# print(myfun("hello wold"))
+
+
+# def myfun():
+#     print("hello world")
+# myfun()
+
+
+# def myfun(x):
+#     print(x)
+# myfun("hello world")
+
+
+def myfun():
+    return "hello world"
+print(myfun())
+    
